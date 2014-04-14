ef32e75b367ae126ed3cf18b4972054bc94ae90a
Brentwood Reid & Chris Lind <student@epicodus.com>
2014-04-14 15:14:08 -0700

    add ajax to create and display answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,23 +1,24 @@
 class AnswersController < ApplicationController
+  def index
+    @question = Question.find(params[:question_id])
+    @answers = @question.answers
+    @answer = Answer.new
+  end
 
   def create
     question = Question.find(params[:question_id])
     user = User.find(question.user_id)
-    @answer = question.answers.new(answer_params)
-
-    if @answer.save
-      redirect_to user_question_path(user, question)
-    else
-      redirect_to user_question_path(user, question)
+    @answer = Answer.create(answer_params)
+    respond_to do |format|
+      format.html { redirect_to user_question_path(user, question) }
+      format.js
     end
-
-
   end
 
   private
 
     def answer_params
-      params.require(:answer).permit(:message, :user_id)
+      params.require(:answer).permit(:message, :user_id, :question_id)
     end
 
 end
